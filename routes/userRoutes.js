e8129c5c276fe0011807aca7d844c45ce51914b1
hereisSwapnil <swapnilskumars@gmail.com>
2024-06-26 20:01:52 +0530

    added final functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const userController = require("../controllers/userController");
-const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
